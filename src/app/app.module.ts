28f3db3514ccc698960c4df32543887da02112be
Shady Mohamed <Shady.Mohamed@URANIUM-CORP.COM>
2023-11-14 20:07:39 +0200

    startup compnents/models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { InfoComponent } from './product/info/info.component';
+import { FeaturesComponent } from './product/features/features.component';
+import { ControlsComponent } from './product/controls/controls.component';
+import { SliderComponent } from './product/slider/slider.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    InfoComponent,
+    FeaturesComponent,
+    ControlsComponent,
+    SliderComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/models/product.model.ts
@@ -0,0 +1,15 @@
+import { Color } from "./color.model";
+
+export interface Product {
+    id: number;
+    title: string;
+    description: string,
+    category: string,
+    video: string,
+    price: number,
+    rate: number,
+    size: number[];
+    images?: [];
+    colors : Color[];
+
+}
